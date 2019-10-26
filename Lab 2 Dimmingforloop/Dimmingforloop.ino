e5875e007b98a289de14b414d132fa624113179a
m1fr <54664603+m1fr@users.noreply.github.com>
2019-10-26 14:47:45 +0200

    Update Dimmingforloop.ino
    Auto Dimme et lys med en for loop, og dimme ned igjen

--- Lab 2 Dimmingforloop/Dimmingforloop.ino
+++ Lab 2 Dimmingforloop/Dimmingforloop.ino
@@ -1,14 +1,12 @@
 //Dimme et lys med en for loop, og dimme ned igjen
-//
+
 const int ledPin = 7;
 
 void setup() {
   pinMode (ledPin, OUTPUT);
-
 }
 
 void loop() {
- 
 //for i in range(255):
 for(int i = 0; i <= 255; i++){
   analogWrite(ledPin, i);
@@ -17,5 +15,5 @@ for(int i = 0; i <= 255; i++){
 for(int i =255; i >=0; i--) {
   analogWrite (ledPin, i) ;
   delay(10);
-}
+  }
 }
